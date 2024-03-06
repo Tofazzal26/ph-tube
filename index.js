364f0043340e3cf4aecef39684b31392656b65cc
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-06 16:47:42 +0600

    button add finish

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+const buttondDataLoad = async () => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/categories`
+  );
 
+  const data = await response.json();
+  const categories = data.data;
+  const btnContainer = document.getElementById("btn-container");
+  categories.forEach((button) => {
+    console.log();
+    const newButton = document.createElement("button");
+    newButton.classList.add(
+      "btn",
+      "btn-ghost",
+      "bg-slate-700",
+      "text-white",
+      "text-lg"
+    );
+    newButton.innerText = `${button.category}`;
+    btnContainer.appendChild(newButton);
+  });
+};
 
-
-
-
-
+buttondDataLoad();
